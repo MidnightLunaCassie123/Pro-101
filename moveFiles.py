82c588b1743e9a7ade12917f494980faf74e0113
MidnightLunaCassie123 <127367938+MidnightLunaCassie123@users.noreply.github.com>
2024-05-15 18:46:20 -0600

    Add files via upload

---
+++ moveFiles.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+from_dir="G:/Other computers/My Laptop/BYJUs coding class work/class 102/C102_assets-main/assets"
+to_dir="G:/Other computers/My Laptop/BYJUs coding class work/class 102/C102_assets-main/assets"
+
+list_of_file=os.listdir(from_dir)
+
+for file_name in list_of_file:
+    name,extension=os.path.splitext(file_name)
+
+    if extension=="":
+        continue
+    if extension in [".txt",".pdf",".doc",".docx"]:
+        path1=from_dir+"/"+file_name
+        path2=to_dir+"/"+"Document_files"
+        path3=to_dir+"/"+"Document_files"+"/"+file_name
+
+        if os.path.exists(path2):
+            print("Moving "+file_name+"....")
+            shutil.move(path1,path3)
+        else:
+            os.makedirs(path2)
+            print("Moving "+file_name+"....")
+            shutil.move(path1,path3)
